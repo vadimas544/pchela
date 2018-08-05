793d729ab7eb55ded69e7b168fddf0755c9175a9
Vadim Samoilenko <vadim123544@gmail.com>
2018-08-05 20:42:17 +0300

    All done

--- mail.php
+++ mail.php
@@ -2,26 +2,40 @@
 	require('db.php');
 	$errors = [];
 	if(isset($_POST['submit'])){
-		if(htmlspecialchars(trim($_POST['email'])) == '' )
+		if(htmlspecialchars(trim($_POST['card'])) == '' )
 	    {
-	        $errors[]='Введите email!';
+	        $errors[]='Введите номер карты!';
 	    }
 
-	    $email = htmlspecialchars(trim($_POST['email']));
-		$sql = "SELECT code_client FROM pos.client_property WHERE code_property = '4' AND value_property='".$email."'";
-		$result = pg_query($sql) or die('Ошибка запроса: ' . pg_last_error());
-				//echo $sql;
-		if(pg_num_rows($result) == 0){
-			$errors[] = 'Указанного почтового ящика не существует!';
-		}
+	    $card = htmlspecialchars(trim($_POST['card']));
 
-				$code_client = pg_fetch_array($result, null,  PGSQL_NUM);
-				$cod = $code_client[0];
+		//$sql = "SELECT code_client FROM pos.client_property WHERE code_property = '4' AND value_property='".$email."'";
+		$begin = '0001';
+     	//$query ="SELECT code_client FROM pos.barcode_client WHERE barcode='".$data['card']."'";
+        $query ="SELECT code_client FROM pos.barcode_client WHERE barcode LIKE '".$begin . $_POST['card'] ."_'";
+        $result = pg_query($query) or die('Ошибка запроса: ' . pg_last_error());
+        if(pg_num_rows($result) == 0){
+            $errors[] = 'Пользователя с такой картой не найдено!';
+        }
+		//$result = pg_query($sql) or die('Ошибка запроса: ' . pg_last_error());
+				//echo $sql;
+		//if(pg_num_rows($result) == 0){
+			//$errors[] = 'Указанного почтового ящика не существует!';
+		//}
+
+				
 
 	    if(empty($errors)){
 	    		
 				//echo $code;
+	    		$code_client = pg_fetch_array($result, null,  PGSQL_NUM);
+				$cod = $code_client[0];
 
+				$sql = "SELECT value_property FROM pos.client_property WHERE code_property = '4' AND code_client='".$cod."'";
+				$res = pg_query($sql) or die('Ошибка запроса: ' . pg_last_error());
+
+				$arr = pg_fetch_array($res, null,  PGSQL_NUM);
+				$email = $arr[0];
 				$gen_pass = substr(md5(time()), 0, 6);
 				//echo $new_pass;
 				$password = password_hash($gen_pass,  PASSWORD_DEFAULT);
@@ -40,7 +54,7 @@
 				//$message = iconv('CP1251', 'UTF-8', $message);
 				mail($to, $subject, $message, $headers);
 
-				echo "Новый пароль сгенерирован и отправлен вам на e-mail!<hr />";
+				echo '<div style="color: green;">'.'Новый пароль сгенерирован и отправлен вам на e-mail!'.'</div><hr />';
 	    }else{
 	    	echo '<div style="color: red;">'.array_shift($errors).'</div><hr>';
 	    }
@@ -51,8 +65,8 @@
 <title>Восстановление пароля</title>
 <form method="post" action="">
 	<p>
-		<label>Ваш e-mail:</label>
-		<input type="text" name="email" />
+		<label>Ваш номер карты:</label>
+		<input type="number" name="card" />
 	</p>
 	<p>
 		<input type="submit" name="submit" value="Восстановить">
--- sms_recovery.php
+++ sms_recovery.php
@@ -4,15 +4,18 @@ require('db.php');
 $errors = [];
 
 if(isset($_POST['submit'])){
-		if(htmlspecialchars(trim($_POST['phone'])) == '' )
+		if(htmlspecialchars(trim($_POST['card'])) == '' )
 	    {
-	        $errors[]='Введите телефон!';
+	        $errors[]='Введите номер карты!';
 	    }
+	    /*
 	    $pos = strstr($_POST['phone'], '8', true);
 	    if($pos != '3'){
 	        $errors[] = 'Неправильный формат номера, нужно 380*********!';
 	    }
-	    $phone = htmlspecialchars(trim($_POST['phone']));
+	    */
+	    /*
+	    $card = htmlspecialchars(trim($_POST['card']));
 		$sql = "SELECT code_client FROM pos.client_property WHERE code_property = '2' AND value_property='".$phone."'";
 		$result = pg_query($sql) or die('Ошибка запроса: ' . pg_last_error());
 				//echo $sql;
@@ -20,12 +23,26 @@ if(isset($_POST['submit'])){
 			$errors[] = 'Указанного номера не существует!';
 		}
 
-				$code_client = pg_fetch_array($result, null,  PGSQL_NUM);
-				$cod = $code_client[0];
+				
+		*/
+		$begin = '0001';
+     	//$query ="SELECT code_client FROM pos.barcode_client WHERE barcode='".$data['card']."'";
+        $query ="SELECT code_client FROM pos.barcode_client WHERE barcode LIKE '".$begin . $_POST['card'] ."_'";
+        $result = pg_query($query) or die('Ошибка запроса: ' . pg_last_error());
+        if(pg_num_rows($result) == 0){
+            $errors[] = 'Пользователя с такой картой не найдено!';
+        }
 
 	    if(empty($errors)){
 	    		
-				//echo $code;
+	    		$code_client = pg_fetch_array($result, null,  PGSQL_NUM);
+				$cod = $code_client[0];
+
+				$sql = "SELECT value_property FROM pos.client_property WHERE code_property = '2' AND code_client='".$cod."'";
+				$res = pg_query($sql) or die('Ошибка запроса: ' . pg_last_error());
+
+				$arr = pg_fetch_array($res, null,  PGSQL_NUM);
+				$phone = $arr[0];
 	    		$user = 'vadim123544@gmail.com';
 				$password = 'Memfis123';
 				$send_sms_url = 'https://esputnik.com/api/v1/message/sms';
@@ -60,7 +77,7 @@ if(isset($_POST['submit'])){
 				send_request($send_sms_url, $json_value, $user, $password);
 
 
-					echo "Новый пароль сгенерирован и отправлен вам на телефон!<hr />";
+					echo '<div style="color: green;">'.'Новый пароль сгенерирован и отправлен вам на телефон!'.'</div><hr />';
 				}
 				else{
 	    	echo '<div style="color: red;">'.array_shift($errors).'</div><hr>';
@@ -94,8 +111,8 @@ if(isset($_POST['submit'])){
 <title>Восстановление пароля</title>
 <form method="post" action="">
 	<p>
-		<label>Ваш номер телефона:</label>
-		<input type="text" name="phone" placeholder="38067*******" />
+		<label>Ваш номер карты:</label>
+		<input type="text" name="card" />
 	</p>
 	<p>
 		<input type="submit" name="submit" value="Восстановить">
