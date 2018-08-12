8bf05c79698fb211593e3cdc0932861db926fefc
Vadim Samoilenko <vadim123544@gmail.com>
2018-08-12 22:18:04 +0300

    The end

--- bonus.php
+++ bonus.php
@@ -5,8 +5,12 @@ $sql ="SELECT * FROM pos.barcode_client WHERE code_client='".$code."'";
 $res = pg_query($sql) or die('Ошибка запроса: ' . pg_last_error());
 $barcode = pg_fetch_array($res, null, PGSQL_ASSOC);
 
+$sql1 = "SELECT code_account FROM pos.client_account WHERE code_client='".$code."'";
+$res1 = pg_query($sql1) or die('Ошибка запроса: ' . pg_last_error());
+$data1 = pg_fetch_array($res1, null, PGSQL_NUM);
+//echo $data1[0];
 
-$sql2 = "SELECT sum_account FROM pos.account WHERE code_account='".$code."'";
+$sql2 = "SELECT sum_account FROM pos.account WHERE code_account='".$data1[0]."'";
 $res2 = pg_query($sql2) or die('Ошибка запроса: ' . pg_last_error());
 $data = pg_fetch_array($res2, null, PGSQL_NUM);
 //echo $data[0];
